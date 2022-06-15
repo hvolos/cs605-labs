537a9668c8cdd364d2400771aaf83ce954519071
hvolos01 <hvolos01@node0.hvolos01-127809.ucy-coast-pg0.wisc.cloudlab.us>
2022-06-15 13:07:08 -0500

    frontend script test

--- websearch/scripts/frontend.sh
+++ websearch/scripts/frontend.sh
@@ -1,92 +1,52 @@
 #!/bin/bash
 
-source common.config
+#VERBOSE=-v
 
-tomcat_instance_name() {
- if [ $i -eq 0 ]; then 
-   echo "tomcat"
- else
-   echo "tomcat"${i}
- fi
+INVENTORY_FILE=hosts
+
+SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
+
+function frontend_hosts() {
+  local inventory_file=$1
+  awk -v TARGET=frontend -F ' *= *' '
+    {
+      if ($0 ~ /^\[.*\]$/) {
+        gsub(/^\[|\]$/, "", $0)
+        SECTION=$0
+      } else if (SECTION==TARGET) {
+        host_port=8080
+        where_host = match($0, /^([A-Za-z0-9\-]*)/, arr_host)
+        where_ansible_host = match($0, /ansible_host=([A-Za-z0-9\-]*)/, arr_ansible_host)
+        where_socket = match($0, /socket=([0-9]*)/, arr_socket)
+        if (where_socket != 0)
+          host_port += arr_socket[1]
+        if (where_ansible_host != 0)
+          host = arr_ansible_host[1]
+        else
+          host = arr_host[1]
+          printf "%s:%d\n", host, host_port
+      }
+    }
+    ' ${inventory_file}
 }
 
-config() {
-  local i=1
-  local port
-  local tomcat_instance_dir
-  while [ $i -lt ${FRONTEND_INSTANCES_PER_SERVER} ];
-  do
-    tomcat_instance_dir=${WEBSEARCH_HOME_DIR}/"tomcat"${i}
-    cp -r ${WEBSEARCH_HOME_DIR}/tomcat ${tomcat_instance_dir}
-    ((http_port=8080+$i))
-    ((ajp_port=8009+$i))
-    ((redirect_port=8443+$i))
-    ((server_port=8005+200+$i))
-    sed -i "/Connector port=\"8080\" protocol=\"HTTP\/1\.1\"/ {n;n; /redirectPort=\"8443\"/ {s/redirectPort=\"8443\"/redirectPort=\"${redirect_port}\"/}}" ${tomcat_instance_dir}/conf/server.xml
-    sed -i "s/Connector port=\"8080\" protocol=\"HTTP\/1\.1\"/Connector port=\"${http_port}\" protocol=\"HTTP\/1\.1\"/" ${tomcat_instance_dir}/conf/server.xml
-    sed -i "s/Connector port=\"8009\" protocol=\"AJP\/1\.3\" redirectPort=\"8443\"/Connector port=\"${ajp_port}\" protocol=\"AJP\/1\.3\" redirectPort=\"${redirect_port}\"/" ${tomcat_instance_dir}/conf/server.xml
-    sed -i "s/Server port=\"8005\" shutdown=\"SHUTDOWN\"/Server port=\"${server_port}\" shutdown=\"SHUTDOWN\"/" ${tomcat_instance_dir}/conf/server.xml
-    ((i=i+1))	
-  done
+function config() {
+  ansible-playbook ${VERBOSE} -i hosts ${SCRIPT_HOME}/ansible/frontend_config.yml
 }
 
-test_frontend() {
-  local i=0
-  local http_port
-  while [ $i -lt ${FRONTEND_INSTANCES_PER_SERVER} ];
-  do	
-    echo "Test frontend server ${FRONTEND_SERVER} instance ${i}..."
-    ((http_port=8080+$i))
-    curl "${FRONTEND_SERVER}:${http_port}/onlyHits.jsp?query=google"
-    echo "Test frontend server ${FRONTEND_SERVER} instance ${i}...DONE"
-    ((i=i+1))	
+function start() {
+  ansible-playbook ${VERBOSE} -i hosts ${SCRIPT_HOME}/ansible/frontend_start.yml
+}
+
+function stop() {
+  ansible-playbook ${VERBOSE} -i hosts ${SCRIPT_HOME}/ansible/frontend_stop.yml
+}
+
+function test() {
+  for frontend in $(frontend_hosts ${INVENTORY_FILE}); do
+    echo "Test frontend server instance ${frontend}..."
+    curl "${frontend}/onlyHits.jsp?query=google"
   done  
-}
-
-start() {
-  local i=0
-  while [ $i -lt ${FRONTEND_INSTANCES_PER_SERVER} ];
-  do
-    echo "Starting frontend server ${FRONTEND_SERVER} instance ${i}..."
-    
-    local tomcat_instance_dir=${WEBSEARCH_HOME_DIR}/$(tomcat_instance_name $i)
-
-    ssh $FRONTEND_SERVER "
-      cd ${tomcat_instance_dir}/logs; 
-      rm catalina.out;
-      export JAVA_HOME=${WEBSEARCH_HOME_DIR}/jdk1.7.0_11; 
-      ${tomcat_instance_dir}/bin/startup.sh &> /dev/null &  exit"
-    
-    ((i=i+1))
-  done
-  
-  #sleep 20
-  while ! nc -z localhost 8080; do sleep 0.1; done;
-
-  i=0
-  while [ $i -lt ${FRONTEND_INSTANCES_PER_SERVER} ];
-  do
-    echo "Starting frontend server ${FRONTEND_SERVER} instance ${i}...DONE"
-    ((i=i+1))
-  done
-
-  test_frontend
-}
-
-stop() {
-  local username=$(whoami)
-
-  local i=0
-  while [ $i -lt ${FRONTEND_INSTANCES_PER_SERVER} ];
-  do
-    echo "Stopping frontend server ${FRONTEND_SERVER} instance ${i}..."
-    
-    local tomcat_instance_dir=${WEBSEARCH_HOME_DIR}/$(tomcat_instance_name $i)
-    ssh ${FRONTEND_SERVER} "ps aux | grep $(tomcat_instance_name $i) | grep -v grep | awk '{print \$2}' | xargs kill -9 2> /dev/null" 
-    
-    ((i=i+1))
-  done
- 
 }
 
 $@
